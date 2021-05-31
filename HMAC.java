daacc0a11b33a33ab432cb4c45c317a328ffa692
Muhammad Asad Shakeel <m.asadshakeel@gmail.com>
2021-06-01 00:31:35 +0500

    - added method to calculate hex string value from hash code

--- HMAC.java
+++ HMAC.java
@@ -10,6 +10,8 @@ public class HMAC {
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
         String secretKey = "This is the secret";
         String hashMessage = "This is the hash message";
+        System.out.println(HMAC.hash(secretKey, hashMessage, "HmacSHA256"));
+        System.out.println(HMAC.hashAndGetHexStringValue(secretKey, hashMessage, "HmacSHA256"));
         System.out.println(HMAC.hashAndConvertToBase64String(secretKey, hashMessage, "HmacSHA256"));
     }
 
@@ -22,6 +24,19 @@ public class HMAC {
         Mac hmac = Mac.getInstance(algorithm);
         hmac.init(secretKey);
         return hmac.doFinal(messageBytes);
+    }
+
+    public static String hashAndGetHexStringValue(String key, String message, String algorithm) throws NoSuchAlgorithmException, InvalidKeyException {
+        byte[] hash = hash(key, message, algorithm);
+        StringBuilder hashVal = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xFF & b);
+            if (hex.length() == 1) {
+                hashVal.append('0');
+            }
+            hashVal.append(hex);
+        }
+        return hashVal.toString();
     }
 
     public static String hashAndConvertToBase64String(String key, String message, String algorithm) throws NoSuchAlgorithmException, InvalidKeyException {
